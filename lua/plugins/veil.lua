2339de5cdee8eecd91869dbe503b0572f07750fd
DZZc0rd <lenovoigoria@gmail.com>
2025-11-12 15:07:09 +0200

    replaced telescope with fzf-lua

--- lua/plugins/veil.lua
+++ lua/plugins/veil.lua
@@ -1,12 +1,22 @@
 return {
     "willothy/veil.nvim",
     dependencies = {
-        "nvim-telescope/telescope.nvim",
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-file-browser.nvim",
     },
     lazy = false,
     opts = {
         startup = true,
     },
+    -- config = function(opts)
+    --     local builtin = require("veil.builtin")
+    --     local sections = {
+    --         builtin.section.animated(builtin.headers.frames_nvim),
+    --     }
+    --
+    --     table.insert(opts, {
+    --         sections,
+    --     })
+    --
+    --     require("veil").setup(opts)
+    -- end,
 }
