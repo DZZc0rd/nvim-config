96509163358a39ca2131360c1a85279d35b4b04b
DZZc0rd <lenovoigoria@gmail.com>
2026-02-06 19:52:06 +0200

    migrate fyler.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@ local keymap = vim.keymap
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+keymap.set("n", "<leader>e", "<cmd>Fyler<cr>", { desc = "Open file explorer" })
--- lua/plugins/fyler.lua
+++ lua/plugins/fyler.lua
@@ -1,35 +1,28 @@
-return {
-    "A7Lavinraj/fyler.nvim",
-    dependencies = { "nvim-mini/mini.icons" },
-    keys = {
-        { "<leader>ee", "<cmd>Fyler<cr>", desc = "Open file explorer" },
-    },
-    opts = {
-        views = {
-            finder = {
-                default_explorer = true,
-                indentscope = {
-                    markers = {
-                        { "┊", "FylerIndentMarker" },
+require("fyler").setup({
+    views = {
+        finder = {
+            default_explorer = true,
+            indentscope = {
+                markers = {
+                    { "┊", "FylerIndentMarker" },
+                },
+            },
+            win = {
+                border = "single",
+                kind = "float",
+                kinds = {
+                    float = {
+                        width = "24%",
+                        height = "100%",
+                        top = "1",
+                        left = "1",
                     },
                 },
-                win = {
-                    border = "single",
-                    kind = "float",
-                    kinds = {
-                        float = {
-                            width = "24%",
-                            height = "100%",
-                            top = "1",
-                            left = "1",
-                        },
-                    },
-                    win_opts = {
-                        cursorline = true,
-                        foldcolumn = "2",
-                    },
+                win_opts = {
+                    cursorline = true,
+                    foldcolumn = "2",
                 },
             },
         },
     },
-}
+})
