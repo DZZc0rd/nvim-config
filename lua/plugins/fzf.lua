76b4808fefcf53f98b1f50418627deec9445d17d
DZZc0rd <lenovoigoria@gmail.com>
2026-02-06 20:22:09 +0200

    migrate fzf-lua

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,16 +1,8 @@
-return {
-    "ibhagwan/fzf-lua",
-    keys = {
-        { "<leader>ff", "<cmd>FzfLua files<cr>",     desc = "Find file" },
-        { "<leader>fb", "<cmd>FzfLua buffers<cr>",   desc = "Find buffer" },
-        { "<leader>fs", "<cmd>FzfLua live_grep<cr>", desc = "Find string" },
-    },
-    opts = {
-        winopts = {
+require("fzf-lua").setup({
+    winopts = {
+        border = "single",
+        preview = {
             border = "single",
-            preview = {
-                border = "single",
-            },
         },
     },
-}
+})
