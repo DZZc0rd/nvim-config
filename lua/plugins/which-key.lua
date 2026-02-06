1320c7124964b7e80492949e4d2b3379a7bca60a
DZZc0rd <lenovoigoria@gmail.com>
2026-02-06 20:27:39 +0200

    add file keymaps group

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,7 +1,11 @@
-require("which-key").setup({
+local wk = require("which-key")
+
+wk.setup({
     win = {
         width = { min = 20, max = 90 },
         row = -2,
         col = 2,
     },
 })
+
+wk.add({ "<leader>f", group = "Find" })
