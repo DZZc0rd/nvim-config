caf4d5d07d7b03c4e415eb9477c5ad1b31ded5d0
DZZc0rd <lenovoigoria@gmail.com>
2026-02-07 11:32:37 +0200

    migrate trouble.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,5 @@ keymap.set("n", "<leader>e", "<cmd>Fyler<cr>", { desc = "Open file explorer" })
 keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find file" })
 keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Find buffers" })
 keymap.set("n", "<leader>fs", "<cmd>FzfLua live_grep<cr>", { desc = "Find string" })
+
+keymap.set("n", "<leader>x", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,8 +1,1 @@
-return {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-    keys = {
-        { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-    },
-    opts = {},
-}
+require("trouble").setup()
